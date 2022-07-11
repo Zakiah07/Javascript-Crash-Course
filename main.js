f929d2358fef5f479fab71558c92f8056bfb7650
Zakiah07 <zakiah_bloom@yahoo.com>
2022-07-11 23:37:14 +0800

    JavaScript Crach Course

---
+++ main.js
@@ -0,0 +1,150 @@
+// Log s statement using console.log()
+// console.log('Hello from main.js')
+
+
+// let age = 25
+// console.log(age)
+
+// // const salary = 80000
+// // console.log(salary)
+
+// let sum = 0
+// sum = 5
+// console.log(sum)
+
+// let result
+// console.log(result)
+
+// const res = null
+
+// const data = null
+
+// const person = {
+//     firstName: 'Bruce',
+//     lastName: 'Wayne',
+//     age: 30,
+// }
+
+// console.log(person.firstName)
+
+// const oddNumbers = [1, 3, 5, 7, 9]
+// console.log(oddNumbers[0])	
+
+// let a = 10
+// a = 'Zakiah'
+// a = true
+// console.log(a)
+
+// let x = 10
+// let y = 5
+
+// console.log(x * y)
+
+// console.log(x != y)
+
+// const isValidNumber = x > 20 || 8 > y
+// console.log(isValidNumber)
+
+// const isValid = false
+// console.log(!isValid)
+
+// console.log('Bruce' + 'Wayne')
+
+// const isEven = 10 % 2 === 0 ? 'Number is even' : 'Number is odd'
+// console.log(isEven)
+
+// console.log(2 + '3')
+// console.log('Bruce' - 'Wayne')
+// console.log('3' * '2')
+// console.log('12' / '3')
+
+// console.log('5' - true)
+
+// console.log(parseFloat('3.4'))
+// console.log((500).toString())
+// console.log(Boolean(10))
+
+// const var1 = 0
+// const var2 = ''
+
+// console.log(var1 == var2)
+// console.log(var1 === var2)
+
+// const num = 0
+
+// if(num > 0) {
+//     console.log('Number is positive')
+// } else if (num < 0) {
+//     console.log('Number is negative')
+// } else {
+//     console.log('Number is zero')
+// }
+
+// const color = '10'
+
+// switch(color){
+//     case 'red':
+//         console.log('Color is red')
+//         break
+//     case 'blue':
+//         console.log('Color is blue')
+//         break
+//     case 'green':
+//         console.log('Color is green')
+//         break
+//         default:
+//             console.log('Not a valid color')
+// }
+
+// for(let i = 1; i <= 5; i++) {
+//     console.log('Iteration number ' + i)
+// }
+
+// let i = 1
+// while (i <=5) {
+//     console.log('Iteration number ' + i)
+//     i++
+// }
+
+// let i = 6
+// do {
+//     console.log('Iteration number ' + i)
+//     i++
+// } while (i <= 5)
+
+// const numArray = [1, 2, 3, 4, 5]
+
+// for (const num of numArray) {
+//     console.log('Iteration number ' + num)
+// }
+
+// function greet(username) {
+//     console.log('Good morning ' + username)
+// }
+
+// function add(a, b) {
+//     return a + b
+// }
+
+// const arrowSum = (a, b) => a + b
+// const addFive = (num) => num + 5
+
+// const sum = arrowSum(25, 25)
+// console.log(sum)
+
+const myNum = 100
+
+if (true) {
+    const myName = 'Zakiah'
+    console.log(myName)
+    console.log(myNum)
+}
+
+function testFn() {
+    const myName = 'Batman'
+    console.log(myName)
+    console.log(myNum)
+}
+
+testFn()
+
